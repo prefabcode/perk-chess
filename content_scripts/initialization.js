969fb78faddc0854889425212430c332119b9702
prefabcode <prefabcode@gmail.com>
2026-01-24 13:32:34 -0500

    initialization logic

--- content_scripts/initialization.js
+++ content_scripts/initialization.js
@@ -9,7 +9,7 @@ import {
   browser, 
   CURRENT_VERSION 
 } from './constants.js';
-import { getCompletedBoards, getCurrentHue, setCompletedBoards, setCurrentHue } from './storageManagement.js';
+import { getCompletedBoards, getCurrentHue, setCompletedBoards, setCurrentHue, setSelectedUnlockOrder } from './storageManagement.js';
 
 function createOnboardingModal() {
   const dialog = document.createElement('dialog');
@@ -60,6 +60,7 @@ export const initializeExtension = async () => {
   createOnboardingModal();
   await setCompletedBoards(0);
   await setCurrentHue(0);
+  await setSelectedUnlockOrder(0);
 
   return new Promise((resolve) => {
     browser.storage.local.set({ initialized: true }, () => {
