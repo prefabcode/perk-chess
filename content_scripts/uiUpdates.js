2853e80a8d09f041b4d6660f81a6a34c5a10de8a
prefabcode <prefabcode@gmail.com>
2026-01-25 13:23:44 -0500

    update perks header when swapping specialization

--- content_scripts/uiUpdates.js
+++ content_scripts/uiUpdates.js
@@ -203,6 +203,7 @@ const setPerkModalEventHandlers = async () => {
     await setImageSources();
     await setActivePerkEventHandler();
     await updatePerksModalContent();
+    await updatePerksHeader();
   });
 
   await setActivePerkEventHandler();
